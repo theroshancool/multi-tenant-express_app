793f8b55949d6bbb081d26425c51705b1e48e755
Roshan sharma <theroshancool@gmail.com>
2025-08-26 11:47:58 +0530

    filesRemoved

--- controller.js
+++
@@ -1,7 +0,0 @@
-// export const userLogin = (req, res) => {
-//     res.send('this is user login')
-// }
-
-// export const userSignup = (req, res) => {
-//     res.send('this is user signup')
-// }
--- route.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express'
-// import { userLogin, userSignup } from './controller.js'
-
-// const router = express.Router()
-
-// router.get('/login',userLogin)
-// router.get('/signup',userSignup)
-
-// export default router
